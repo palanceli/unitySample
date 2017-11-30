77d48e1d4c81bac96c290cdae01d33a06fd78968
palanceli <palanceli@163.com>
2017-11-30 21:42:57 +0800

    commit changes

--- AndroidRollABall/app/src/main/java/com/sample/palance/rollaball/MainActivity.java
+++ AndroidRollABall/app/src/main/java/com/sample/palance/rollaball/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sample.palance.rollaball;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +8,25 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    protected UnityPlayer mUnityPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button scanButton = (Button)findViewById(R.id.scanbutton);
-        scanButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Toast.makeText(MainActivity.this,
-                        "正在启动扫描程序，请稍等...", Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
+        mUnityPlayer = new UnityPlayer(this);
+        ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.layout);
+        layout.addView(mUnityPlayer);
+
+//        Button scanButton = (Button)findViewById(R.id.scanbutton);
+//        scanButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Toast.makeText(MainActivity.this,
+//                        "正在启动扫描程序，请稍等...", Toast.LENGTH_LONG)
+//                        .show();
+//            }
+//        });
     }
 }
