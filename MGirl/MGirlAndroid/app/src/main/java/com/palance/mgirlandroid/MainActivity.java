bff7b5de96601b61d0a7c3c4e8c92107b7ad9d2f
palanceli <palanceli@163.com>
2017-12-02 14:25:50 +0800

    commit changes

--- MGirl/MGirlAndroid/app/src/main/java/com/palance/mgirlandroid/MainActivity.java
+++ MGirl/MGirlAndroid/app/src/main/java/com/palance/mgirlandroid/MainActivity.java
@@ -1,13 +1,32 @@
 package com.palance.mgirlandroid;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.unity3d.player.UnityPlayer;
 
 public class MainActivity extends AppCompatActivity {
+    protected UnityPlayer mUnityPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUnityPlayer = new UnityPlayer(this);
+        ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.mainLayout);
+        layout.addView(mUnityPlayer);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mUnityPlayer.resume();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
     }
 }
--- MGirl/cpfiles.sh
+++ MGirl/cpfiles.sh
@@ -1,9 +1,10 @@
 currDir=$(cd "$(dirname "$0")"; pwd)
 
-unityName="MGirlUnity"
-androidName="MGirlAndroid"
+projectName="MGirl"
+unityName=$projectName"Unity"
+androidName=$projectName"Android"
 
-cp -f $unityName/build/$unityName/libs/unity-classes.jar $androidName/libs/unity-classes.jar
-cp -rf $unityName/build/$unityName/src/main/assets/bin/ $androidName/app/src/main/assets/bin/
-cp -rf $unityName/build/$unityName/src/main/jniLibs/ $androidName/app/src/main/jniLibs/
+cp -f $unityName/build/$projectName/libs/unity-classes.jar $androidName/app/libs/unity-classes.jar
+cp -rf $unityName/build/$projectName/src/main/assets/bin/ $androidName/app/src/main/assets/bin/
+cp -rf $unityName/build/$projectName/src/main/jniLibs/ $androidName/app/src/main/jniLibs/
 #cp -f build/Roll\ a\ Ball/src/main/java/com/sample/palance/rollaball/UnityPlayerActivity.java ../AndroidRollABall/app/src/main/java/com/sample/palance/rollaball/UnityPlayerActivity.java
